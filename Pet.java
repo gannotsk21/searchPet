7114bfee3abbe88eb13cadc1d7fdbc0ece4c889b
Polina Gannotskaya <42845358+gannotsk21@users.noreply.github.com>
2022-03-09 14:08:40 -0800

    Add files via upload
    base files

---
+++ Pet.java
@@ -0,0 +1,15 @@
+public Class pet{//Id Name Type Gender ZipCode
+    int id;
+    String name;
+    String type;
+    String gender;
+    int zipcode;
+    public class pet(int id, String name, String Type, String gender, int zipcode){
+        this.id = id;
+        this.name = name;
+        this.type = type;
+        this.gender = gender;
+        this.zipcode = zipcode;
+    }
+    
+}
